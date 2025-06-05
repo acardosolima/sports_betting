5f0dd7d13eae055d81ce7d6198e68a8b11186b45
adriano.lima <adriano.lima@ab-inbev.com>
2025-06-05 12:19:30 -0300

    Chore: setup codeql

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="SSA",
+    name="Sports Betting",
     version="0.0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
